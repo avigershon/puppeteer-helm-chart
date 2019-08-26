38f6b608f207a1ce948ec061d3971c7fe649086f
avigershon <avigershon@gmail.com>
2019-08-26 14:06:32 +0300

    changed to Europe region

--- setup-gcp.sh
+++ setup-gcp.sh
@@ -81,7 +81,7 @@ system_setup () {
 
     # echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
 
-    curl https://storage.googleapis.com/kubernetes-helm/helm-v2.13.0-linux-amd64.tar.gz > helm.tar.gz
+    curl https://storage.googleapis.com/kubernetes-helm/helm-v2.14.0-linux-amd64.tar.gz > helm.tar.gz
     tar -xvzf helm.tar.gz
     sudo cp linux-amd64/* /usr/local/bin
     rm -rf helm.tar.gz
@@ -261,7 +261,7 @@ gcp_create_cluster () {
 
   CLUSTER_NAME=$1
 
-  gcloud beta container --project "crawler-249816" clusters create "${CLUSTER_NAME}" --zone "us-central1-a" --username "admin" --cluster-version "1.12.8-gke.10" --machine-type "n1-standard-2" --image-type "COS" --disk-type "pd-standard" --disk-size "10" --scopes "https://www.googleapis.com/auth/cloud-platform" --preemptible --num-nodes "3" --enable-cloud-logging --enable-cloud-monitoring --no-enable-ip-alias --network "projects/crawler-249816/global/networks/default" --subnetwork "projects/crawler-249816/regions/us-central1/subnetworks/default" --enable-autoscaling --min-nodes "3" --max-nodes "4" --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair
+  gcloud beta container --project "crawler-249816" clusters create "${CLUSTER_NAME}" --zone "europe-west1-b" --username "admin" --cluster-version "1.12.8-gke.10" --machine-type "n1-standard-2" --image-type "COS" --disk-type "pd-standard" --disk-size "10" --scopes "https://www.googleapis.com/auth/cloud-platform" --preemptible --num-nodes "3" --enable-cloud-logging --enable-cloud-monitoring --no-enable-ip-alias --network "projects/crawler-249816/global/networks/default" --subnetwork "projects/crawler-249816/regions/us-central1/subnetworks/default" --enable-autoscaling --min-nodes "3" --max-nodes "4" --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair
 
 }
 
